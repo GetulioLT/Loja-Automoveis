43f7395bd709abe7a7f3d32169994887cd0a97e8
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2023-06-29 19:11:54 -0300

    0.0.3
    criado a conexão utilizando o php e um banco RDS

---
+++ PHP/automoveis.php
@@ -0,0 +1,37 @@
+<?php
+include ("infos.php");
+
+// Conexão com o banco de dados
+$conn = new mysqli($host, $username, $password, $database);
+
+// Verificar se houve erro na conexão
+if ($conn->connect_error) {
+    die("Erro ao conectar ao banco de dados: " . $conn->connect_error);
+}
+
+// Consulta SQL para selecionar todos os registros da tabela "automoveis"
+$sql = "SELECT * FROM automoveis";
+$result = $conn->query($sql);
+
+// Verificar se há resultados retornados pela consulta
+if ($result->num_rows > 0) {
+    // Array para armazenar os resultados da consulta
+    $data = array();
+
+    // Iterar sobre os resultados e armazená-los em um array
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+
+    // Converter o array em formato JSON
+    $json_data = json_encode($data);
+
+    // Imprimir os dados no formato JSON
+    echo $json_data;
+} else {
+    echo "Nenhum resultado encontrado.";
+}
+
+// Fechar a conexão com o banco de dados
+$conn->close();
+?>
---
+++ PHP/concessionarias.php
@@ -0,0 +1,37 @@
+<?php
+include ("infos.php");
+
+// Conexão com o banco de dados
+$conn = new mysqli($host, $username, $password, $database);
+
+// Verificar se houve erro na conexão
+if ($conn->connect_error) {
+    die("Erro ao conectar ao banco de dados: " . $conn->connect_error);
+}
+
+// Consulta SQL para obter os dados da tabela "concessionarias"
+$sql = "SELECT * FROM concessionarias";
+$result = $conn->query($sql);
+
+// Verificar se há resultados da consulta
+if ($result->num_rows > 0) {
+    // Array para armazenar os resultados da consulta
+    $data = array();
+
+    // Loop através dos resultados e armazenar em um array associativo
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+
+    // Converter o array em formato JSON
+    $json_data = json_encode($data);
+
+    // Imprimir os dados no formato JSON
+    echo $json_data;
+} else {
+    echo "Nenhum resultado encontrado.";
+}
+
+// Fechar a conexão com o banco de dados
+$conn->close();
+?>
