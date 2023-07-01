04ae5d819ab1603ee86b750b5147a80da8600cdd
GetulioLT <getulio.santos.lesstime@gmail.com>
2023-07-01 11:10:02 -0300

    senha ec2

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "web-conce.cyp6jjti2v9z.sa-east-1.rds.amazonaws.com";
+$username = "jason";
+$password = "X3qczvx6i5qJwt0XOmeq";
+$database = "db_conce";
+?>
