3b04bcdb186227d6c4f0741b29a839fabad7d248
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2023-07-08 10:44:52 -0300

    Update infos.php

--- PHP/infos.php
+++ PHP/infos.php
@@ -1,7 +1,7 @@
 <?php
 // Configuração do banco de dados
-$host = "web-conce.cyp6jjti2v9z.sa-east-1.rds.amazonaws.com";
+$host = "concessionaria.cyp6jjti2v9z.sa-east-1.rds.amazonaws.com";
 $username = "jason";
-$password = "X3qczvx6i5qJwt0XOmeq";
+$password = "ehysuRkvhYqchNpoc2WG";
 $database = "db_conce";
 ?>
